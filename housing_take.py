8f45ce995c03dd384472fc3ac7584acb9e6f0dad
mycow2 <101349307+mycow2@users.noreply.github.com>
2025-01-13 16:01:44 -0500

    Update housing_take.py

--- housing_take.py
+++ housing_take.py
@@ -6,17 +6,18 @@ Created on Fri Jan 10 17:07:42 2025
 """
 #%%
 import pandas as pd
+import numpy as np
 #%% example links
 #https://www.nyc.gov/assets/finance/downloads/pdf/rolling_sales/annualized-sales/2010/2010_manhattan.xls
 #https://www.nyc.gov/assets/finance/downloads/pdf/rolling_sales/annualized-sales/2017/2017_manhattan.xls
 #%% getting the data
-def get_housing(year): 
+def get_housing(borough,year): 
     if year > 2017: 
         excel = 'xlsx'
     else: 
         excel = 'xls'
     
-    base = pd.read_excel(f'https://www.nyc.gov/assets/finance/downloads/pdf/rolling_sales/annualized-sales/{str(year)}/{str(year)}_manhattan.{excel}')
+    base = pd.read_excel(f'https://www.nyc.gov/assets/finance/downloads/pdf/rolling_sales/annualized-sales/{str(year)}/{str(year)}_{borough}.{excel}')
     base.columns = ['borough', 'neighborhood', 'building_class_category',
            'tax_class_at_present', 'block', 'lot', 'ease-ment',
            'building_class_at_present', 'address', 'apartment_number', 'zip_code',
@@ -40,7 +41,7 @@ def get_housing(year):
 
 housing_dict = {}
 for year in range(2010,2024): 
-    housing_dict[year] = get_housing(year)
+    housing_dict[year] = get_housing('manhattan',year)
 #%% concat
 all_housing = pd.concat(housing_dict.values())
 all_housing = all_housing.dropna(subset = 'neighborhood')
@@ -48,10 +49,19 @@ all_housing = all_housing[~all_housing['borough'].isin(['BOROUGH','BOROUGH\n'])]
 
 all_housing['sale_date'] = pd.to_datetime(all_housing['sale_date'])
 all_housing['year'] = all_housing['sale_date'].dt.year
+#%% address cleaning
+def clean_apt(address): 
+    if len(address.split(",")) > 1: #if splittable
+        apt = address.split(",")[-1]
+    else: 
+        apt = address
+    return apt
+#%% Cleaning Apartment Number
+all_housing['apartment_number'] = all_housing['apartment_number'].str.strip()
+all_housing['apartment_number'] = all_housing['apartment_number'].astype(str)
 
-#%% exporting
-all_housing.to_csv(r'\\owg.ds.corp\serverfarm\KnowledgeBase\Health\0-Training\2024\New Hire Orientation\Work\MT\P Projects\all_housing_nyc.csv')
-
-#%%
-sample =all_housing.groupby('year').sample(frac=0.01, random_state=42) 
-dupe_sales = all_housing[all_housing.duplicated(subset = ['block','lot'],keep=False)].sort_values(subset = ['block','lot'])
+#where apt number is empty or nan
+all_housing['apartment_number'] = np.where(all_housing['apartment_number'].isin(['','nan']),all_housing['address'].apply(clean_apt),all_housing['apartment_number'])
+all_housing['apartment_number'] = all_housing['apartment_number'].str.strip()
+all_housing['lot'] = all_housing['lot'].astype(str).str.strip()
+all_housing['block'] = all_housing['block'].astype(str).str.strip()
